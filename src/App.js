b3038abbc8345df8c7872c56d0cd500186b02173
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-11-04 13:35:37 +0100

    New Expense added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -6,32 +8,31 @@ function App() {
       id: "e1",
       title: "Laptop",
       amount: 1250.33,
-      date: new Date(2021, 0, 1)
+      date: new Date(2021, 0, 1),
     },
     {
       id: "e2",
       title: "New TV",
       amount: 350.39,
-      date: new Date(2021, 4, 9)
+      date: new Date(2021, 4, 9),
     },
     {
       id: "e3",
       title: "Car insurance",
       amount: 150.93,
-      date: new Date(2021, 2, 15)
+      date: new Date(2021, 2, 15),
     },
     {
       id: "e4",
       title: "Tires",
       amount: 350.19,
-      date: new Date(2021, 10, 23)
+      date: new Date(2021, 10, 23),
     },
-  ]
-
+  ];
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
