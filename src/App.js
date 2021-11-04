1b6a531de9b49fdf9862706c9a273bc08a21483e
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-11-04 16:49:52 +0100

    Expense filter added

--- src/App.js
+++ src/App.js
@@ -3,39 +3,44 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Laptop",
-      amount: 1250.33,
-      date: new Date(2021, 0, 1),
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 350.39,
-      date: new Date(2021, 4, 9),
-    },
-    {
-      id: "e3",
-      title: "Car insurance",
-      amount: 150.93,
-      date: new Date(2021, 2, 15),
-    },
-    {
-      id: "e4",
-      title: "Tires",
-      amount: 350.19,
-      date: new Date(2021, 10, 23),
-    },
-  ];
+    const expenses = [
+        {
+            id: "e1",
+            title: "Laptop",
+            amount: 1250.33,
+            date: new Date(2021, 0, 1),
+        },
+        {
+            id: "e2",
+            title: "New TV",
+            amount: 350.39,
+            date: new Date(2021, 4, 9),
+        },
+        {
+            id: "e3",
+            title: "Car insurance",
+            amount: 150.93,
+            date: new Date(2021, 2, 15),
+        },
+        {
+            id: "e4",
+            title: "Tires",
+            amount: 350.19,
+            date: new Date(2021, 10, 23),
+        },
+    ];
 
-  return (
-    <div>
-      <NewExpense />
-      <Expenses items={expenses} />
-    </div>
-  );
+    const addExpenseHandler = (expense) => {
+        console.log("In App.js");
+        console.log(expense);
+    };
+
+    return (
+        <div>
+            <NewExpense onAddExpense={addExpenseHandler} />
+            <Expenses items={expenses} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,36 +1,82 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredAmount, setEnteredAmount] = useState("");
     const [enteredDate, setEnteredDate] = useState("");
+
     //all states default values are STRING because when we read data from event.target value it will always return STRING VALUE
+
+    /*  const [userInput, setUserInput] = useState({
+        enteredTitle: "",
+        enteredAmount: "",
+        enteredDate: "",
+    }); */
+    //IF WE WANT TO HAVE ONE STATE with an object
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
+        /*  setUserInput((userInput) => {
+            return {
+                ...userInput, //takes an object, pulls out key value pairs and adds them to this new object
+                enteredTitle: event.target.value, //this override key value pair like enteredTitle in this case
+            };
+        }); */
     };
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
+        /* setUserInput((userInput) => {
+            return {
+                ...userInput,
+                enteredAmount: event.target.value,
+            };
+        }); */
     };
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+        /* setUserInput((userInput) => {
+            return {
+                ...userInput,
+                enteredDate: event.target.value,
+            };
+        }); */
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault(); //when we click on form submit browser default behavior is to refresh page, we need to prevent this.
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle("");
+        setEnteredAmount("");
+        setEnteredDate("");
     };
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input
+                        type="text"
+                        value={enteredTitle} //This is called TWO-WAY BINDING
+                        onChange={titleChangeHandler}
+                    />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Amount</label>
                     <input
                         type="number"
+                        value={enteredAmount}
                         min="0.01"
                         step="0.01"
                         onChange={amountChangeHandler}
@@ -41,6 +87,7 @@ const ExpenseForm = () => {
                     <label>Date</label>
                     <input
                         type="date"
+                        value={enteredDate}
                         min="2019-01-01"
                         max="2022-12-31"
                         onChange={dateChangeHandler}
